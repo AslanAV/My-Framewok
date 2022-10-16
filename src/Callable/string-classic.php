50f0e0c274863a27909e126472dac270d63fec66
AslanAV <autlevaslan@yandex.ru>
2022-10-16 07:25:58 +0300

    delete filis

--- src/Callable/string-classic.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// callable строки
-$callableString = 'mb_strtolower';
-
-assert(true === is_callable($callableString));
-
-$argument = 'HIGH';
-printf("\nТест анонимной функции callableString('%s'): %s\n\n", $argument, $callableString($argument));
